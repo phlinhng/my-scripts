636ec9568ee4fc2d1d6aa71211f970c55d593e94
phlinhng <phlinhng@outlook.com>
2020-05-04 21:18:58 +0800

    update swap.sh

--- sh/swap.sh
+++ sh/swap.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
 if [[ $(/usr/bin/id -u) -ne 0 ]]; then
-    echo "must be root user"
-    exit 2
+  sudoCmd="sudo"
+else
+  sudoCmd=""
 fi
 
 # allocate space
-fallocate -l 1G /swapfile
+${sudoCmd} fallocate -l 1G /swapfile
 
 # set permission
-chmod 600 /swapfile
+${sudoCmd} chmod 600 /swapfile
 
 # make swap
-mkswap /swapfile
+${sudoCmd} mkswap /swapfile
 
 # enable swap
-swapon /swapfile
+${sudoCmd} swapon /swapfile
 
 # make swap permanent
-echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
+printf "/swapfile swap swap defaults 0 0" | ${sudoCmd} tee -a /etc/fstab  >/dev/null
 
 # set swap percentage
-sysctl vm.swappiness=10
-echo "vm.swappiness=10" >> /etc/sysctl.conf
+${sudoCmd} sysctl vm.swappiness=10
+printf "vm.swappiness=10" | ${sudoCmd} tee -a /etc/sysctl.conf >/dev/null
 
 free -h
 
