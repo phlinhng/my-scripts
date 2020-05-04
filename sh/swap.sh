e699cfe92b7fee2472c8207b4d625f551988b4a8
phlinhng <phlinhng@outlook.com>
2020-05-04 21:22:13 +0800

    set condition for swap.sh

--- sh/swap.sh
+++ sh/swap.sh
@@ -6,25 +6,29 @@ else
   sudoCmd=""
 fi
 
-# allocate space
-${sudoCmd} fallocate -l 1G /swapfile
+if [[ ! $(cat /proc/swaps | wc -l) -gt 1 ]]; then
+  # allocate space
+  ${sudoCmd} fallocate -l 1G /swapfile
 
-# set permission
-${sudoCmd} chmod 600 /swapfile
+  # set permission
+  ${sudoCmd} chmod 600 /swapfile
 
-# make swap
-${sudoCmd} mkswap /swapfile
+  # make swap
+  ${sudoCmd} mkswap /swapfile
 
-# enable swap
-${sudoCmd} swapon /swapfile
+  # enable swap
+  ${sudoCmd} swapon /swapfile
 
-# make swap permanent
-printf "/swapfile swap swap defaults 0 0" | ${sudoCmd} tee -a /etc/fstab  >/dev/null
+  # make swap permanent
+  printf "/swapfile swap swap defaults 0 0" | ${sudoCmd} tee -a /etc/fstab  >/dev/null
 
-# set swap percentage
-${sudoCmd} sysctl vm.swappiness=10
-printf "vm.swappiness=10" | ${sudoCmd} tee -a /etc/sysctl.conf >/dev/null
+  # set swap percentage
+  ${sudoCmd} sysctl vm.swappiness=10
+  printf "vm.swappiness=10" | ${sudoCmd} tee -a /etc/sysctl.conf >/dev/null
 
-free -h
+  free -h
+else
+  free -h
+fi
 
 exit 0
